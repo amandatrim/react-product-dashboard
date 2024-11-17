ac6a32f8b25c76444e52f471f16068fec0833e1c
amandatrim <amanda162@usf.edu>
2024-11-17 05:01:58 +0100

    Create ProductList component

---
+++ src/ProductList.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+/* Import productItem from the product item module  */
+import ProductItem from './ProductItem';
+
+const ProductList = ({ products }) => {
+    return (
+        <div>
+            <h2>Product List</h2>
+            {products.map((product) => (
+                /* pass the props to the ProductItem component */
+                <ProductItem key={product.id} product={product} />
+            ))}
+        </div>
+    );
+};
+
+/* Export the ProductList component */
+
+export default ProductList;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+/* Import app from the app module */
 import App from './App';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
+      {/* Render the app under react strict mode */}
         <App />
     </React.StrictMode>
 );
