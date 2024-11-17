f333bb2e5af019b13ef353d1a33f572a00b8ade7
amandatrim <amanda162@usf.edu>
2024-11-17 05:21:25 +0100

    Connect AddProductForm to App with data-passing functionality

--- src/App.jsx
+++ src/App.jsx
@@ -17,8 +17,7 @@ const App = () => {
         setProducts([
             ...products, 
             { ...product, id: Date.now() }, 
-        ]);
-    };
+        ]);};
 
     return (
         <div>
